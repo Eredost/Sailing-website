c191d73d15c9ffdc41d3442ba3af1d27bdc03ef6
Eredost <michael.coutin62@gmail.com>
2020-01-23 13:46:19 +0100

    Adding Scrollex functionality on the navigation bar

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,39 @@ require('jquery.scrollex');
 var app = {
   init: function() {
     console.log('init');
+    app.watchSection();
+  },
+
+  watchSection: function() {
+    $('#intro').scrollex({
+      mode: 'middle',
+      enter: function () {
+        $('.navbar__item__link[href="#intro"]').addClass('active');
+      },
+      leave: function () {
+        $('.navbar__item__link[href="#intro"]').removeClass('active');
+      }
+    });
+
+    $('#articles').scrollex({
+      mode: 'middle',
+      enter: function () {
+        $('.navbar__item__link[href="#articles"]').addClass('active');
+      },
+      leave: function () {
+        $('.navbar__item__link[href="#articles"]').removeClass('active');
+      }
+    });
+
+    $('#counters').scrollex({
+      mode: 'middle',
+      enter: function () {
+        $('.navbar__item__link[href="#counters"]').addClass('active');
+      },
+      leave: function () {
+        $('.navbar__item__link[href="#counters"]').removeClass('active');
+      }
+    });
   }
 };
 
